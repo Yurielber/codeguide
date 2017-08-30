fc364429db07f9a13cb90d4d6ec3802e4a67b4ae
Edmund <yurielber@gmail.com>
2017-08-30 14:32:14 +0800

    Update ptrTest.sh

--- ptrTest.sh
+++ ptrTest.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 RANGES=" 49.4 "
+SERVER='114.114.114.114'
+
 for x in $RANGES; do
 	for y in {0..255}; do
 		for z in {0..255}; do
 			target="$x.$y.$z"
-			stdout=$(dig @8.8.8.8 "$target" +retry=1 +time=1 +cmd 2>&1)
+			stdout=$(dig @$SERVER "$target" +retry=1 +time=1 +cmd 2>&1)
 			echo "$stdout" | grep ';; Got answer:' >/dev/null 2>&1
 			got_answer=$( echo $?)
 			code=$( echo "$stdout" | sed -rn 's/^[[:print:]]+?, status: ([[:alpha:]]*?), id: [[:digit:]]+/\1/p')
