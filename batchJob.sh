5375afc367f57240d5528ba1768b7b3d575c1a76
Edmund <yurielber@gmail.com>
2017-09-01 17:28:24 +0800

    Update batchJob.sh

--- batchJob.sh
+++ batchJob.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
 ranges="$1"
+start="$2"
+end="$3"
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 EXEC="$DIR/digTest.sh"
 PIDLIST=''
 OUTPUT='dig.log'
-THREADS=10
+THREADS=100
 
 function validateRange
 {
 if [ -z "$ranges" ]; then
-	exit 1
+        exit 1
 fi
 local RANGE_PATTERN='([0-9]{1,3}\.)[0-9]{1,3}'
 if [[ ! "$ranges" =~ ${RANGE_PATTERN} ]]; then
-	echo 'ERROR: invalid ip address' >&2
-	exit 1
+        echo 'ERROR: invalid ip address' >&2
+        exit 1
 fi
 }
 
@@ -24,24 +26,21 @@ function waits
 local finish=false
 while [ $finish = false ]
 do
-	local flag=true
-	for j in $PIDLIST; do
-		if [ $(kill -0 $j >/dev/null 2>&1; echo $?) == 0 ]; then
-			echo "Process [$j] is running"
-			flag=false
-			echo "flag --> $flag"
-			break
-		fi
-	done
-	if [ $flag == false ]; then
-		local WAIT=10
-		echo "wait for $WAIT s"
-		finish=false
-		sleep $WAIT
-	else
-		finish=true
-		PIDLIST=''
-	fi
+        local flag=true
+        for j in $PIDLIST; do
+                if [ $(kill -0 $j >/dev/null 2>&1; echo $?) == 0 ]; then
+                        flag=false
+                        break
+                fi
+        done
+        if [ $flag == false ]; then
+                local WAIT=30
+                finish=false
+                sleep $WAIT
+        else
+                finish=true
+                PIDLIST=''
+        fi
 done
 }
 
@@ -49,15 +48,21 @@ function startJobs
 {
 local START=0
 local END=255
+if [ -n "$start" ]; then
+        START=$start
+fi
+if [ -n "$end" ]; then
+        END=$end
+fi
 for (( i=$START; i<=$END; i++ )); do
-	local target="$ranges.$i"
-	/bin/bash "$EXEC" "$target" &
-	local pid=$(echo "$!")
-	PIDLIST="$PIDLIST $pid"
-	if [ $(( (i+1) % $THREADS )) == 0 ] || [ i == $END ] ; then
-		echo "$PIDLIST"
-		waits
-	fi
+        local target="$ranges.$i"
+        echo "[$target] start ..."
+        /bin/bash "$EXEC" "$target" &
+        local pid=$(echo "$!")
+        PIDLIST="$PIDLIST $pid"
+        if [ $(( (i+1) % $THREADS )) == 0 ] || [ $i == $END ]; then
+                waits
+        fi
 done
 exit 0
 }
